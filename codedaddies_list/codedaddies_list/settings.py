5c2315ac5b9d17b0f73e011c78301037e5ac9289
Atharv Shingewar <shingewaratharv@gmail.com>
2021-06-12 17:13:59 +0530

    added frontend

--- codedaddies_list/codedaddies_list/settings.py
+++ codedaddies_list/codedaddies_list/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+#modified by me-atharv
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+#end of modifications
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'codedaddies_list.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- codedaddies_list/my_app/views.py
+++ codedaddies_list/my_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
+def home(request):
+    return render(request,'base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    context = {'search':search}
+    return render(request,'my_app/new_search.html',context)
